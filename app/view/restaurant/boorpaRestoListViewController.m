6d2e56ff0e16fed3aa9a2162f62f43c94d39ead4
Arie Prasetyo <vanbungkring@Aries-MacBook-Pro.local>
2014-08-01 15:59:31 +0800

    refactor-> add new schema

--- app/view/restaurant/boorpaRestoListViewController.m
+++ app/view/restaurant/boorpaRestoListViewController.m
@@ -37,32 +37,32 @@
         
         table.tableHeaderView = searchbar_wrapper;
         
-       
+        
         
         self.title =@"Bengkel terdekat";
         [self.view addSubview:table];
         // If you want your BarButtonItem to handle touch event and click, use a UIButton as customView
-//        UIButton *customButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 20, 20)];
-//        // Add your action to your button
-//        [customButton addTarget:self action:@selector(barButtonItemPressed:) forControlEvents:UIControlEventTouchUpInside];
-//        // Customize your button as you want, with an image if you have a pictogram to display for example
-//        [customButton setImage:[UIImage imageNamed:@"cart"] forState:UIControlStateNormal];
-//        
-//        // Then create and add our custom BBBadgeBarButtonItem
-//        BBBadgeBarButtonItem *barButton = [[BBBadgeBarButtonItem alloc] initWithCustomUIButton:customButton];
-//        // Set a value for the badge
-//        barButton.badgeValue = @"2";
-//        
-//        barButton.badgeOriginX = 13;
-//        barButton.badgeOriginY = -9;
-//        
-//        // Add it as the leftBarButtonItem of the navigation bar
-//        self.navigationItem.rightBarButtonItem = barButton;
+        //        UIButton *customButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 20, 20)];
+        //        // Add your action to your button
+        //        [customButton addTarget:self action:@selector(barButtonItemPressed:) forControlEvents:UIControlEventTouchUpInside];
+        //        // Customize your button as you want, with an image if you have a pictogram to display for example
+        //        [customButton setImage:[UIImage imageNamed:@"cart"] forState:UIControlStateNormal];
+        //
+        //        // Then create and add our custom BBBadgeBarButtonItem
+        //        BBBadgeBarButtonItem *barButton = [[BBBadgeBarButtonItem alloc] initWithCustomUIButton:customButton];
+        //        // Set a value for the badge
+        //        barButton.badgeValue = @"2";
+        //
+        //        barButton.badgeOriginX = 13;
+        //        barButton.badgeOriginY = -9;
+        //
+        //        // Add it as the leftBarButtonItem of the navigation bar
+        //        self.navigationItem.rightBarButtonItem = barButton;
     }
     return self;
 }
 -(void)barButtonItemPressed:(id)sender{
- 
+    
 }
 - (void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:YES];
@@ -75,7 +75,7 @@
     return 1;
 }
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
-
+    
     boorpaDetailRestaurantViewController *detail = [[boorpaDetailRestaurantViewController alloc]init];
     [self.navigationController pushViewController:detail animated:YES];
 }
@@ -87,22 +87,23 @@
     }
     [cell.logo setImage:[UIImage imageNamed:@"thumbs_fr_0062.jpg"]];
     return cell;
-   
+    
 }
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-	return 155*0.5;
+    return 155*0.5;
 }
 - (void)viewDidLoad
 {
-//    [super viewDidLoad];for (NSString* family in [UIFont familyNames])
-//    {
-//        NSLog(@"%@", family);
-//        
-//        for (NSString* name in [UIFont fontNamesForFamilyName: family])
-//        {
-//            NSLog(@"  %@", name);
-//        }
-//    }
+    [super viewDidLoad];
+    //    [super viewDidLoad];for (NSString* family in [UIFont familyNames])
+    //    {
+    //        NSLog(@"%@", family);
+    //
+    //        for (NSString* name in [UIFont fontNamesForFamilyName: family])
+    //        {
+    //            NSLog(@"  %@", name);
+    //        }
+    //    }
     // Do any additional setup after loading the view.
 }
 
@@ -113,14 +114,14 @@
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+ {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
